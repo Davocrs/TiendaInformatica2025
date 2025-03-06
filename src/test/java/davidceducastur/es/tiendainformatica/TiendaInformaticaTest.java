0d398d30fe4253095b817fe90c1569ed690e67f7
alu03d <alu03d@A204PC11.desarrolloweb.local>
2025-03-06 14:04:57 +0100

    Ultima version dia 6 de Marzo

--- src/test/java/davidceducastur/es/tiendainformatica/TiendaInformaticaTest.java
+++ src/test/java/davidceducastur/es/tiendainformatica/TiendaInformaticaTest.java
@@ -14,48 +14,41 @@ import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.Test;
 import static org.junit.jupiter.api.Assertions.*;
 
-/**
- *
- * @author alu03d
- */
 public class TiendaInformaticaTest {
-    
-    public TiendaInformaticaTest() {
-    }
-    
+
+    TiendaInformatica t;
+
     @BeforeAll
     public static void setUpClass() {
-        t.articulos.put("1-11", new Articulo("1-11", "RATON LOGITECH ST ", 14, 15));
-        t.articulos.put("1-22", new Articulo("1-22", "TECLADO STANDARD  ", 9, 18));
-        t.articulos.put("2-11", new Articulo("2-11", "HDD SEAGATE 1 TB  ", 16, 80));
-        t.articulos.put("2-22", new Articulo("2-22", "SSD KINGSTOM 256GB", 9, 70));
-        t.articulos.put("2-33", new Articulo("2-33", "SSD KINGSTOM 512GB", 0, 200));
-        t.articulos.put("3-22", new Articulo("3-22", "EPSON PRINT XP300 ", 5, 80));
-        t.articulos.put("4-11", new Articulo("4-11", "ASUS  MONITOR  22 ", 5, 100));
-        t.articulos.put("4-22", new Articulo("4-22", "HP MONITOR LED 28 ", 5, 180));
-        t.articulos.put("4-33", new Articulo("4-33", "SAMSUNG ODISSEY G5", 12, 580));
     }
-    
+
     @AfterAll
     public static void tearDownClass() {
     }
-    
+
     @BeforeEach
     public void setUp() {
+        t = new TiendaInformatica();
+        t.articulos.put("1-11", new Articulo("1-11", "RATON LOGITECH ST", 14, 15));
+        t.articulos.put("1-22", new Articulo("1-22", "TECLADO STANDARD", 9, 18));
+        t.articulos.put("2-11", new Articulo("2-11", "HDD SEAGATE 1 TB", 16, 80));
+        t.articulos.put("2-22", new Articulo("2-22", "SSD KINGSTOM 256GB", 9, 70));
+        t.articulos.put("2-33", new Articulo("2-33", "SSD KINGSTOM 512GB", 0, 200));
+        t.articulos.put("3-22", new Articulo("3-22", "EPSON PRINT XP300", 5, 80));
+        t.articulos.put("4-11", new Articulo("4-11", "ASUS MONITOR 22", 5, 100));
+        t.articulos.put("4-22", new Articulo("4-22", "HP MONITOR LED 28", 5, 180));
+        t.articulos.put("4-33", new Articulo("4-33", "SAMSUNG ODISSEY G5", 12, 580));
     }
-    
+
     @AfterEach
     public void tearDown() {
     }
-    
-    TiendaInformatica t = new TiendaInformatica();
-    
+
     @Test
     public void testTotalPedido() {
-        
         LocalDate hoy = LocalDate.now();
         System.out.println("totalPedido");
-        
+
         Pedido p1 = new Pedido("80580845T-001/2024", t.getClientes().get("80580845T"), hoy.minusDays(1), new ArrayList<>
         (List.of(new LineaPedido("1-11", 3), new LineaPedido("4-22", 3))));
         Pedido p2 = new Pedido("80580845T-002/2024", t.getClientes().get("80580845T"), hoy.minusDays(2), new ArrayList<>
@@ -66,13 +59,12 @@ public class TiendaInformaticaTest {
         (List.of(new LineaPedido("4-33", 3), new LineaPedido("2-11", 3))));
         Pedido p5 = new Pedido("63921307Y-001/2024", t.getClientes().get("63921307Y"), hoy.minusDays(4), new ArrayList<>
         (List.of(new LineaPedido("2-11", 5), new LineaPedido("2-33", 3), new LineaPedido("4-33", 2))));
-        
+
         assertEquals(585, t.totalPedido(p1));
         assertEquals(2980, t.totalPedido(p2));
         assertEquals(390, t.totalPedido(p3));
         assertEquals(1980, t.totalPedido(p4));
         assertEquals(2160, t.totalPedido(p5));
- 
     }
-    
 }
+
